2fc61918b33c96c85d359e807812fd7cf08491a0
csre7144 <csre7144@gmail.com>
2026-05-20 23:53:54 +0530

    Updated Math Util

---
+++ gradh_util.py
@@ -0,0 +1,14 @@
+def calculate_avg(scores):
+    return sum(scores) / len(scores)
+
+def get_grade(score):
+    if score >= 90:
+        return 'A'
+    elif score >= 80:
+        return 'B'
+    elif score >= 70:
+        return 'C'
+    elif score >= 60:
+        return 'D'
+    else:
+        return 'F' 
--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import math_util
+import math
+import random
 
 print(math_util.add(5, 3))
 print(math_util.subtract(5, 3))
 print(math_util.multiply(5, 3))
+print(math_util.divide(5, 3))
+
+print(math.sqrt(16))
+print(math.pi)
+
+print(random.randint(1, 10))
+
+
+import gradh_util
+
+marks = []
+n = int(input("Enter number of students: "))
+
+for i in range(1, n + 1):
+    num1 = int(input("Enter the marks: "))
+    marks.append(num1)
+
+avg = gradh_util.calculate_avg(marks)
+grade = gradh_util.get_grade(avg)
+
+print("Average Marks:", avg)
+print("Grade:", grade)
